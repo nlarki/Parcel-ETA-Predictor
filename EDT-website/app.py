40081652af0f914e2eff5a1c6c721266bd4e5de0
nlarki <niall_larkin2010@hotmail.com>
2021-11-08 20:53:56 +0000

    ive added the finishing pieces to app.py, thus allowing it to post the results of the models back to the homepage

--- EDT-website/app.py
+++ EDT-website/app.py
@@ -4,8 +4,8 @@ import numpy as np
 import datetime
 
 GB = pickle.load(
-    open('ParcelLab-Task\JupyterNotebook\GradientBoosting.sav', 'rb'))
-RF = pickle.load(open('ParcelLab-Task\JupyterNotebook\RandomForest.sav', 'rb'))
+    open('GradientBoosting.pkl', 'rb'))
+RF = pickle.load(open('RandomForest.pkl', 'rb'))
 
 
 app = Flask(__name__)
@@ -24,12 +24,34 @@ def boosting():
     return_tracking = request.form['d']
     delivery_location = request.form['e']
     Epoch_transit_date = request.form['f']
-    Epoch_created_date = request.form['g']
-    Epoch_pickup_date = request.form['h']
+    Epoch_pickup_date = request.form['g']
     arr = np.array([[delivery_region, courier, zip_code, return_tracking,
-                     delivery_location, Epoch_transit_date, Epoch_created_date, Epoch_pickup_date]])
+                     delivery_location, Epoch_transit_date, Epoch_pickup_date]])
     GBpred = GB.predict(arr)
-    GBpred = datetime.datetime.fromtimestamp(GBpred)
+    GBpred = GBpred.item(0)
+    GBresult = GBpred/1000
+    GBresult = datetime.datetime.fromtimestamp(GBresult)
+
+    return render_template('index.html', pred=format(GBresult))
+
+
+@app.route('/forest', methods=['POST', 'GET'])
+def forest():
+    delivery_region = request.form['1']
+    courier = request.form['2']
+    zip_code = request.form['3']
+    return_tracking = request.form['4']
+    delivery_location = request.form['5']
+    Epoch_transit_date = request.form['6']
+    Epoch_pickup_date = request.form['7']
+    arr = np.array([[delivery_region, courier, zip_code, return_tracking,
+                     delivery_location, Epoch_transit_date, Epoch_pickup_date]])
+    Fpred = RF.predict(arr)
+    Fpred = Fpred.item(0)
+    Fresult = Fpred/1000
+    Fresult = datetime.datetime.fromtimestamp(Fresult)
+
+    return render_template('index.html', pred1=format(Fresult))
 
 
 if __name__ == "__main__":
