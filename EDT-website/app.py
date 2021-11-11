fc2390d6f9bf2b3dec62bfe78952ac8955441687
nlarki <niall_larkin2010@hotmail.com>
2021-11-11 00:05:20 +0000

    multiple changes - had to change the epoch conversion code so that the date conversion on the website would work correctly

--- EDT-website/app.py
+++ EDT-website/app.py
@@ -20,23 +20,21 @@ def home():
 def boosting():
     delivery_region = request.form['a']
     courier = request.form['b']
-    zip_code = request.form['c']
-    return_tracking = request.form['d']
-    delivery_location = request.form['e']
-    Epoch_transit_date = request.form['f']
-    Epoch_pickup_date = request.form['g']
+    return_tracking = request.form['c']
+    delivery_location = request.form['d']
+    Epoch_transit_date = request.form['e']
+    Epoch_pickup_date = request.form['f']
     transformedTransit = np.datetime64(Epoch_transit_date)
     transformedPickup = np.datetime64(Epoch_pickup_date)
     tt = (transformedTransit - np.datetime64('1970-01-01T00:00:00Z')) / \
         np.timedelta64(1, 's')
     tp = (transformedPickup - np.datetime64('1970-01-01T00:00:00Z')) / \
         np.timedelta64(1, 's')
-    arr = np.array([[delivery_region, courier, zip_code, return_tracking,
+    arr = np.array([[delivery_region, courier, return_tracking,
                      delivery_location, tt, tp]])
     GBpred = GB.predict(arr)
     GBpred = GBpred.item(0)
-    GBresult = GBpred/1000
-    GBresult = datetime.datetime.fromtimestamp(GBresult)
+    GBresult = datetime.datetime.fromtimestamp(GBpred)
 
     return render_template('index.html', pred='The estimated delivery date for your item is {}'.format(GBresult))
 
@@ -45,23 +43,21 @@ def boosting():
 def forest():
     delivery_region = request.form['1']
     courier = request.form['2']
-    zip_code = request.form['3']
-    return_tracking = request.form['4']
-    delivery_location = request.form['5']
-    Epoch_transit_date = request.form['6']
-    Epoch_pickup_date = request.form['7']
+    return_tracking = request.form['3']
+    delivery_location = request.form['4']
+    Epoch_transit_date = request.form['5']
+    Epoch_pickup_date = request.form['6']
     transformedTransit = np.datetime64(Epoch_transit_date)
     transformedPickup = np.datetime64(Epoch_pickup_date)
     tt = (transformedTransit - np.datetime64('1970-01-01T00:00:00Z')) / \
         np.timedelta64(1, 's')
     tp = (transformedPickup - np.datetime64('1970-01-01T00:00:00Z')) / \
         np.timedelta64(1, 's')
-    arr = np.array([[delivery_region, courier, zip_code, return_tracking,
+    arr = np.array([[delivery_region, courier, return_tracking,
                      delivery_location, tt, tp]])
     Fpred = RF.predict(arr)
     Fpred = Fpred.item(0)
-    Fresult = Fpred/1000
-    Fresult = datetime.datetime.fromtimestamp(Fresult)
+    Fresult = datetime.datetime.fromtimestamp(Fpred)
 
     return render_template('index.html', pred1='The estimated delivery date for your item is {}'.format(Fresult))
 
